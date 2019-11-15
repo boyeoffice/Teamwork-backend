92fb7b582c813ca737ae0ce2fdea1fe19ccb6db8
Olakunle Boye <kunlexzy@gmail.com>
2019-11-15 10:13:14 +0100

    ch-test-index

--- test/index.js
+++ test/index.js
@@ -3,10 +3,13 @@ const app = require("../app");
 const request = require('supertest');
 
 describe('Test server /index', () => {
-	it('should return response status 200', () => {
+	it('should return response status 200', (done) => {
 		request(app).get('/')
 			.then((res) => {
-				expect(res.statusCode).to.equal(200)
+				expect(res.statusCode).to.equal(200);
+				expect(res.body).to.include.keys('message');
+				 expect(res.body.message).to.equal('Welcome to teamwork project');
+				 done()
 			})
 			.catch((err) => console.log(err))
 	});
