88cfcd8fd30ed6fe060279698bf21d1b03b9aea9
Olakunle Boye <boye@sprinble.co>
2020-07-29 22:35:58 +0100

    commet body parser

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const bodyParser =require('body-parser');
+// const bodyParser =require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extend: true}));
+app.use(express.json());
+// app.use(express.urlencoded({extend: true}));
 
 app.get('/', (req, res) => {
     res.send({
