9ce72c46a4ddda8ee69287dc60a2a9fcccee4435
Olakunle Boye <boye@sprinble.co>
2020-09-02 08:59:58 +0100

    pending

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
-// const bodyParser =require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// use body-parser
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(
+    bodyParser.urlencoded({
+        limit: '50mb',
+        extended: true,
+        parameterLimit: 50000,
+    }),
+);
+
 app.use(express.json());
-// app.use(express.urlencoded({extend: true}));
+app.use(express.urlencoded({ extend: true }));
 
 dotenv.config();
 
