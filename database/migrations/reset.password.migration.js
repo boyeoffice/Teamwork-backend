82327c3a4cd275645369abb0bedf658322db406e
Olakunle Boye <kunlexzy@gmail.com>
2022-12-07 07:24:28 +0100

    reset password unit test

--- database/migrations/reset.password.migration.js
+++ database/migrations/reset.password.migration.js
@@ -7,17 +7,13 @@ const createResetPasswordsTable = () => {
       email VARCHAR (50) NOT NULL,
       token VARCHAR (50) NOT NULL,
       created_on timestamp with time zone NOT NULL
-    )`).then(res => {
-      console.log('Reset passwords table migrated')
-    })
+    )`)
   )
 };
 
 const dropResetPasswordTable = () => {
   return Promise.resolve(
-    db.query('DROP TABLE IF EXISTS reset_passwords').then(res => {
-      console.log('Reset password table dropped')
-    })
+    db.query('DROP TABLE IF EXISTS reset_passwords')
   );
 };
 
