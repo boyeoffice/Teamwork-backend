574bf0ab1d00b4da61068b2e574446a0e70130fd
Olakunle Boye <kunlexzy@gmail.com>
2022-12-09 11:16:14 +0100

    setup production environment

--- database/connect.js
+++ database/connect.js
@@ -2,16 +2,17 @@ const Pool = require('pg-pool');
 
 const env = require('../env');
 
+
 const config = {
   host: env.database_host,
   database: env.database_name,
   user: env.database_user ,
   password: env.database_pass,
-  port: 5432,
+  port: env.database_port,
   ssl: false,
   max: 1, // set pool max size to 20
   idleTimeoutMillis: 1000, // close idle clients after 1 second
-  connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+  // connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
   maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (
 }
 
@@ -19,12 +20,12 @@ const pool = new Pool(config);
 
 function query(queryText, params) {
   return new Promise((resolve, reject) => {
-      pool
-          .query(queryText, params)
-          .then((res) => {
-              resolve(res);
-          })
-          .catch((err) => reject(err));
+    pool
+        .query(queryText, params)
+        .then((res) => {
+            resolve(res);
+        })
+        .catch((err) => reject(err));
   });
 }
 
