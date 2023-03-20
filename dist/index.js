c9b4d9100149c26e4f5a8cc314665cbae90acde6
Olakunle Boye <kunlexzy@gmail.com>
2023-03-20 05:26:45 +0100

    migrate to typescript

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bootstrap_1 = require("./src/bootstrap");
-bootstrap_1.default.listen(5000, () => console.log(`🚀 is running on port ${5000}`));
