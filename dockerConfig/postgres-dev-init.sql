82e6a4e6b2da33f43f303431e6bfd5a1ef16d16c
Olakunle Boye <kunlexzy@gmail.com>
2023-04-08 06:07:50 +0100

    upgraded docker images

---
+++ dockerConfig/postgres-dev-init.sql
@@ -0,0 +1,8 @@
+CREATE USER teamwork with encrypted password 'secret';
+-- Create Dev Database
+-- CREATE DATABASE teamwork_db;
+-- GRANT ALL PRIVILEGES ON DATABASE  teamwork_db TO teamwork;
+
+-- Create Test Database
+CREATE DATABASE teamwork_test_db;
+GRANT ALL PRIVILEGES ON DATABASE  teamwork_test_db TO teamwork;
